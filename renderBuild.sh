ef9ff91af4dd4eaf5672ba08930275e57333d722
Xiong Tianrui <121947202+Ihavenochoised@users.noreply.github.com>
2025-12-24 17:45:36 +0800

    Add IP logging

--- renderBuild.sh
+++ renderBuild.sh
@@ -7,4 +7,7 @@ npm install
 echo "📍 List of installed dependencies:"
 npm ls
 
+echo "📍 Current IP address (add this to MongoDB access control)"
+curl ifconfig.me
+
 echo "✅️ Dependencies installed, ready to start!"
