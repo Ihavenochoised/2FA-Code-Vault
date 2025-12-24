72a7aee291a2df6094fd3affcd1dc72f9aa847b9
Xiong Tianrui <121947202+Ihavenochoised@users.noreply.github.com>
2025-12-24 17:48:57 +0800

    Give some time for user to see the IP address

--- renderBuild.sh
+++ renderBuild.sh
@@ -10,4 +10,10 @@ npm ls
 echo "📍 Current IP address (add this to MongoDB access control)"
 curl ifconfig.me
 
+# Give the user 1 minute to copy the IP address
+echo "⏳ You have 1 minute to copy the IP address above..."
+sleep 55
+echo "⏳ 5 seconds remaining..."
+sleep 5
+
 echo "✅️ Dependencies installed, ready to start!"
